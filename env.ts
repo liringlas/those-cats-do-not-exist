1f7fb5d37a8cd22ee5edba263887a0f807326649
Konstantin Ryzhov <theaklair@gmail.com>
2019-02-22 14:19:54 +0300

    env updated

--- env.ts
+++ env.ts
@@ -1,2 +1,3 @@
-export const INTERVAL = process.env.INTERVAL ? parseInt(process.env.INTERVAL) : 1024;
+export const INTERVAL =
+  process.env.INTERVAL !== undefined && !isNaN(parseInt(process.env.INTERVAL)) ? parseInt(process.env.INTERVAL) : 1024;
 export const URL = process.env.URL || 'https://thiscatdoesnotexist.com/';
